671775de38f463ede934160786d95eabc9662ba6
Hardews <2021214014@stu.cqupt.edu.cn>
2021-12-06 16:04:32 +0800

    feat(api):router init

--- api/rounter.go
+++ api/rounter.go
@@ -4,12 +4,30 @@ import "github.com/gin-gonic/gin"
 
 func InitEngine() {
 	engine := gin.Default()
-	/*
+
 		userGroup := engine.Group("/user")
+		{
+			userGroup.POST("/register",Register)
+			userGroup.POST("/login",Login)
+			userGroup.POST("/password",replacePassword)
+
+			userGroup.GET("/info",getInfo)  //获取个人信息
+			userGroup.POST("/info",replaceInfo)  //更改个人信息
+		}
 
 		PostGroup := engine.Group("/post")
+		{
+			PostGroup.POST("/")   //发留言
+			PostGroup.GET("/:id")   //获取留言
+			PostGroup.DELETE("/") //删除留言
+		}
 
 		comment   := engine.Group("/comment")
-	*/
+		{
+			comment.POST("/") //发表评论
+			comment.GET("/:id")  //获取评论
+			comment.DELETE("/") //删除评论
+		}
+
 	engine.Run()
 }
--- api/user.go
+++ api/user.go
@@ -9,3 +9,15 @@ func Login(c *gin.Context)  {
 func Register(c *gin.Context)  {
 
 }
+
+func replacePassword(c *gin.Context)  {
+
+}
+
+func getInfo(c *gin.Context)  {
+
+}
+
+func replaceInfo(c *gin.Context)  {
+
+}
